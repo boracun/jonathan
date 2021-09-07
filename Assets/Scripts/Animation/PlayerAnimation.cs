3b99f2cec2e29109d0f0fccd801fd0130ddd5228
boracun <cun2424@gmail.com>
2021-09-07 13:30:37 +0300

    Add player animation for player 1

---
+++ Assets/Scripts/Animation/PlayerAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Animation
+{
+    public class PlayerAnimation : MonoBehaviour
+    {
+        public SpriteRenderer spriteRenderer;
+        public Animator animator;
+        public Rigidbody2D rb;
+
+        private void Update()
+        {
+            if (rb.velocity.x < 0) spriteRenderer.flipX = true;
+            else if (rb.velocity.x > 0) spriteRenderer.flipX = false;
+            
+            float direction = 0f;
+
+            if (rb.velocity.x != 0 || rb.velocity.y != 0)
+                direction = 1f;
+            else
+                direction = 0f;
+            
+            animator.SetFloat("direction", direction);
+        }
+    }
+}
